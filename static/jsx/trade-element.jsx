a0ce1dd9adfc05f5b78fe220331f958b07c94e02
jacksing <hijackyeah@163.com>
2016-08-01 07:00:18 +0800

    Updated random text generator
    Random text generator for specified length.

--- static/jsx/trade-element.jsx
+++ static/jsx/trade-element.jsx
@@ -2,11 +2,51 @@ var React = require('react');
 var ReactDom = require('react-dom');
 var $ = require('jquery');
 
-var HelloMessage = React.createClass({
-    render: function() {
-        return <h1>Hello {this.props.name}</h1>
-    },
-});
+var fullWidthChars = require('./full-width-chars');
+
+function hasValue(value) {
+    return !(value == null || value == undefined || value == '');
+}
+
+function randomString(length, mix=true, byte=false) {
+    var randomChineseChar = () => {
+        // parts of chinese char field.
+        var charFrom = 19968,
+            charTo = 20968,
+            maxLen = charTo - charFrom;
+        return String.fromCharCode(charFrom + Math.floor(Math.random() * maxLen));
+    };
+
+    var randomFullWidthChar = () => {
+        return fullWidthChars.chinese[Math.floor(Math.random() * fullWidthChars.chinese.length)];
+    };
+
+    var randomHalfChar = () => {
+        var strRepo = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+        return strRepo.charAt(Math.floor(Math.random() * strRepo.length));
+    };
+
+    var rest = length;
+    var resultStr = '';
+
+    while (true) {
+        if (rest == 0) {
+            break;
+        }
+
+        var getHalfChar = (!mix || rest == 1) || Math.random() > 0.5;
+
+        if (getHalfChar && false) {
+            resultStr += randomHalfChar();
+            rest--;
+        }
+        else {
+            resultStr += randomFullWidthChar();
+            rest -= byte ? 2 : 1;
+        }
+    }
+    return resultStr;
+}
 
 var TradePattern = React.createClass({
     propTypes: {
@@ -15,34 +55,63 @@ var TradePattern = React.createClass({
     getInitialState: function () {
         return {
             edage: this.props.edage,
+            indrop: this.props.edage - 1,
+            outdrop: this.props.edage + 1,
+            indropText: randomString(this.props.edage - 1),
+            edageText: randomString(this.props.edage),
+            outdropText: randomString(this.props.edage + 1),
         }
     },
     handleChange: function (event) {
+        var value = event.target.value;
+        if (!hasValue(value)) {
+            this.setState({
+                edage: '',
+                indrop: '',
+                outdrop: '',
+                indropText: '',
+                edageText: '',
+                outdropText: '',
+            });
+            return;
+        }
+        if (isNaN(value) || value > this.props.max || value < this.props.min) {
+            return;
+        }
+        var edage = parseInt(value);
         this.setState({
-            edage: event.target.value,
+            edage: edage,
+            indrop: edage - 1,
+            outdrop: edage + 1,
+            indropText: randomString(edage - 1),
+            edageText: randomString(edage),
+            outdropText: randomString(edage + 1),
         });
     },
     render: function () {
         return (
             <div className="trade-pattern">
-                <input type="text" ref="indrop" value={this.state.edage - 1} readOnly />
                 <input type="text" ref="edage" value={this.state.edage} onChange={this.handleChange} />
-                <input type="text" ref="outdrop" value={this.state.edage + 1} readOnly />
+                <div>
+                    <span>{this.state.indrop}</span>
+                    <textarea name="" id="" cols="100" rows="5" value={this.state.indropText} readOnly></textarea>
+                </div>
+                <div>
+                    <span>{this.state.edage}</span>
+                    <textarea name="" id="" cols="100" rows="5" value={this.state.edageText} readOnly></textarea>
+                </div>
+                <div>
+                    <span>{this.state.outdrop}</span>
+                    <textarea name="" id="" cols="100" rows="5" value={this.state.outdropText} readOnly></textarea>
+                </div>
             </div>
         );
     },
 });
 
-// $(function() {
-//     ReactDom.render(
-//         <HelloMessage name="jacksing" />,
-//         document.getElementById("example")
-//     );
-// });
-
 $(function() {
     ReactDom.render(
-        <TradePattern edage={10} />,
+        <TradePattern edage={10} max={1000} min={1} />,
         document.getElementById("example")
     );
 });
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,10 @@ module.exports = {
         loaders: [{
             test: /\.js$/,
             exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
         }, {
             test: /\.jsx$/,
             exclude: /(node_modules|bower_components)/,

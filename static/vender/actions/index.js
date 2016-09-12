bc68509c6772bf209cf2d963e3c11e49855bbbaf
Jacksing <hijackyeah@163.com>
2016-09-12 17:38:06 +0800

    Fillin meta detail actoins

--- static/vender/actions/index.js
+++ static/vender/actions/index.js
@@ -1,13 +1,29 @@
-export const addMetaDetail = () => {
+import {ADD_META_DETAL, MOD_META_DETAIL, DEL_META_DETAIL} from '../constants/MetaDetailActions'
+import {ADD_PROPERTY, DEL_PROPERTY} from '../constants/PropertyActions'
 
+export const addMetaDetail = (code, name, description) => {
+    return {
+        type: ADD_META_DETAL,
+        name,
+        description,
+    }
 }
 
-export const modMetaDetail = () => {
-
+export const modMetaDetail = (code, value, name, description) => {
+    return {
+        type: MOD_META_DETAIL,
+        value,
+        name,
+        description,
+    }
 }
 
-export const delMetaDetail = () => {
-
+export const delMetaDetail = (code, value) => {
+    return {
+        type: DEL_META_DETAIL,
+        code,
+        value,
+    }
 }
 
 export const addProperty = () => {
